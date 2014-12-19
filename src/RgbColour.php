06a015be8643567c711f5afd2ee26291888a13cf
Chris Throup <chris@throup.org.uk>
2014-12-19 23:47:27 +0000

    Added simple behaviour to RgbColour class: implemented equals() method.

--- src/RgbColour.php
+++ src/RgbColour.php
@@ -8,5 +8,61 @@ namespace colourspace;
  * @package colourspace
  */
 class RgbColour {
+    /**
+     * @param float $red
+     * @param float $green
+     * @param float $blue
+     */
+    public function __construct($red = 0.0, $green = 0.0, $blue = 0.0) {
+        $this->red   = (float) $red;
+        $this->green = (float) $green;
+        $this->blue  = (float) $blue;
+    }
 
+    /**
+     * @return float
+     */
+    public function getRed() {
+        return (float) $this->red;
+    }
+
+    /**
+     * @return float
+     */
+    public function getGreen() {
+        return (float) $this->green;
+    }
+
+    /**
+     * @return float
+     */
+    public function getBlue() {
+        return (float) $this->blue;
+    }
+
+    /**
+     * @param  self $second
+     *
+     * @return bool
+     */
+    public function equals(self $second) {
+        return ($this->getRed()   == $second->getRed())
+            && ($this->getGreen() == $second->getGreen())
+            && ($this->getBlue()  == $second->getBlue());
+    }
+
+    /**
+     * @type float
+     */
+    private $red   = 0.0;
+
+    /**
+     * @type float
+     */
+    private $green = 0.0;
+
+    /**
+     * @type float
+     */
+    private $blue  = 0.0;
 }
--- tests/RgbColourTest.php
+++ tests/RgbColourTest.php
@@ -14,4 +14,26 @@ class RgbColourTest extends \PHPUnit_Framework_TestCase {
     public function init() {
         new RgbColour();
     }
+
+    /**
+     * @test
+     */
+    public function equal() {
+        $first  = new RgbColour(1, 0, 0);
+        $second = new RgbColour(1, 0, 0);
+
+        $this->assertTrue($first->equals($second));
+        $this->assertTrue($second->equals($first));
+    }
+
+    /**
+     * @test
+     */
+    public function notEqual() {
+        $first  = new RgbColour(1, 0, 0);
+        $second = new RgbColour(0, 0, 1);
+
+        $this->assertFalse($first->equals($second));
+        $this->assertFalse($second->equals($first));
+    }
 }
