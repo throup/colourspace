2989e5b9d9f019f53e2a06862c9993bc4efee095
Chris Throup <chris@throup.org.uk>
2014-12-21 16:04:23 +0000

    Extract creation of colour objects (within tests) into a utility method.

--- tests/RgbColourTest.php
+++ tests/RgbColourTest.php
@@ -14,7 +14,7 @@ class RgbColourTest extends \PHPUnit_Framework_TestCase {
         $green = 0.4;
         $blue  = 0.8;
 
-        $colour = new RgbColour($red, $green, $blue);
+        $colour = $this->generateRgbColour($red, $green, $blue);
 
         $this->assertEquals($red,   $colour->getRed());
         $this->assertEquals($green, $colour->getGreen());
@@ -25,8 +25,8 @@ class RgbColourTest extends \PHPUnit_Framework_TestCase {
      * @test
      */
     public function equal() {
-        $first  = new RgbColour(1, 0, 0);
-        $second = new RgbColour(1, 0, 0);
+        $first  = $this->generateRgbColour(1, 0, 0);
+        $second = $this->generateRgbColour(1, 0, 0);
 
         $this->assertTrue($first->equals($second));
         $this->assertTrue($second->equals($first));
@@ -36,10 +36,28 @@ class RgbColourTest extends \PHPUnit_Framework_TestCase {
      * @test
      */
     public function notEqual() {
-        $first  = new RgbColour(1, 0, 0);
-        $second = new RgbColour(0, 0, 1);
+        $first  = $this->generateRgbColour(1, 0, 0);
+        $second = $this->generateRgbColour(0, 0, 1);
 
         $this->assertFalse($first->equals($second));
         $this->assertFalse($second->equals($first));
+    }
+
+    /**
+     * Generate a testable colour object for the provided RGB representation.
+     *
+     * @param float $red   The red component of this colour's RGB
+     *                     representation; in the range [0.0–1.0].
+     * @param float $green The green component of this colour's RGB
+     *                     representation; in the range [0.0–1.0].
+     * @param float $blue  The blue component of this colour's RGB
+     *                     representation; in the range [0.0–1.0].
+     *
+     * @return RgbColour
+     */
+    protected function generateRgbColour($red   = 0.0,
+                                         $green = 0.0,
+                                         $blue  = 0.0) {
+        return new RgbColour($red, $green, $blue);
     }
 }
