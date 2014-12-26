e8ef2727d7d3d46c02687c7a66cc3e29a09b16ca
Chris Throup <chris@throup.org.uk>
2014-12-26 22:44:37 +0000

    Basic implementation of matrix multiplication.

--- src/Matrix.php
+++ src/Matrix.php
@@ -9,14 +9,19 @@
 
 namespace Colourspace;
 
+use ArrayAccess;
 use ArrayIterator;
+use Traversable;
 
 /**
  * Class Matrix
  */
 class Matrix extends ArrayIterator {
     /**
-     * @param self $second
+     * Returns a new Matrix object resulting from the addition of this one
+     * with the second.
+     *
+     * @param self $second The matrix to be added.
      *
      * @return self
      */
@@ -27,11 +32,14 @@ class Matrix extends ArrayIterator {
         }
         return new self($new);
     }
+
     /**
-     * @param $row
-     * @param $row2
+     * Utility method to return the sum of two matrix rows.
      *
-     * @return array
+     * @param Traversable|float[] $row  The first row.
+     * @param ArrayAccess|float[] $row2 The second row.
+     *
+     * @return float[]
      */
     protected function addRows($row, $row2) {
         $newrow = [];
@@ -41,12 +49,45 @@ class Matrix extends ArrayIterator {
         return $newrow;
     }
 
+    /**
+     * The number of rows within this matrix.
+     *
+     * @return int
+     */
     public function rows() {
         return count($this);
     }
 
+    /**
+     * The number of columns within this matrix.
+     *
+     * @return int
+     */
     public function columns() {
         $row = reset($this);
         return count($row);
+    }
+
+    /**
+     * Returns a new Matrix object resulting from the matrix multiplication of
+     * this one with the second.
+     *
+     * @param self $second The matrix by which this will be multiplied.
+     *
+     * @return self
+     */
+    public function product(self $second) {
+        $rows = $second->rows();
+        $cols = $second->columns();
+        $data = [];
+        for ($i = 0; $i < $rows; $i++) {
+            for ($j = 0; $j < $cols; $j++) {
+                $data[$i][$j] = 0;
+                for ($k = 0; $k < $rows; $k++) {
+                    $data[$i][$j] += $this[$i][$k] * $second[$k][$j];
+                }
+            }
+        }
+        return new self($data);
     }
 }
--- tests/MatrixTest.php
+++ tests/MatrixTest.php
@@ -98,4 +98,191 @@ class MatrixTest extends PHPUnit_Framework_TestCase {
 
         $this->assertEquals(3, $matrix->columns());
     }
+
+    /**
+     * @test
+     */
+    public function productOfTwoEmptyMatrices_returnsCorrectValue() {
+        $first = new Matrix(
+            []
+        );
+        $second = new Matrix(
+            []
+        );
+        $expected = new Matrix(
+            []
+        );
+
+        $result = $first->product($second);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function productOfTwo1x1Matrices_returnsCorrectValue() {
+        $first = new Matrix(
+            [
+                [2],
+            ]
+        );
+        $second = new Matrix(
+            [
+                [3],
+            ]
+        );
+        $expected = new Matrix(
+            [
+                [6],
+            ]
+        );
+
+        $result = $first->product($second);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function productOfTwo1x1IdentityMatrices_returnsCorrectValue() {
+        $first = new Matrix(
+            [
+                [1],
+            ]
+        );
+        $second = new Matrix(
+            [
+                [1],
+            ]
+        );
+        $expected = new Matrix(
+            [
+                [1],
+            ]
+        );
+
+        $result = $first->product($second);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function productOfTwo2x2IdentityMatrices_returnsCorrectValue() {
+        $first = new Matrix(
+            [
+                [1, 0],
+                [0, 1],
+            ]
+        );
+        $second = new Matrix(
+            [
+                [1, 0],
+                [0, 1],
+            ]
+        );
+        $expected = new Matrix(
+            [
+                [1, 0],
+                [0, 1],
+            ]
+        );
+
+        $result = $first->product($second);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function productOfTwo2x2Matrices_returnsCorrectValue() {
+        $first = new Matrix(
+            [
+                [ 1,  2],
+                [ 3,  4],
+            ]
+        );
+        $second = new Matrix(
+            [
+                [ 5,  6],
+                [ 7,  8],
+            ]
+        );
+        $expected = new Matrix(
+            [
+                [19, 22],
+                [43, 50],
+            ]
+        );
+
+        $result = $first->product($second);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function productOfTwo3x3Matrices_returnsCorrectValue() {
+        $first = new Matrix(
+            [
+                [ 1,  2,  3],
+                [ 4,  5,  6],
+                [ 7,  8,  9],
+            ]
+        );
+        $second = new Matrix(
+            [
+                [10, 11, 12],
+                [13, 14, 15],
+                [16, 17, 18],
+            ]
+        );
+        $expected = new Matrix(
+            [
+                [ 84,  90,  96],
+                [201, 216, 231],
+                [318, 342, 366],
+            ]
+        );
+
+        $result = $first->product($second);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function productOfVectorAnd3x3Matrix_returnsCorrectValue() {
+        $matrix = new Matrix(
+            [
+                [ 1,  2,  3],
+                [ 4,  5,  6],
+                [ 7,  8,  9],
+            ]
+        );
+        $vector = new Matrix(
+            [
+                [ 1],
+                [ 2],
+                [ 3],
+            ]
+        );
+        $expected = new Matrix(
+            [
+                [14],
+                [32],
+                [50],
+            ]
+        );
+
+        $result = $matrix->product($vector);
+
+        $this->assertEquals($expected, $result);
+    }
 }
