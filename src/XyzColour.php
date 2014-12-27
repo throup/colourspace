85a17d0cc63ec6e377af2eb5461d6d33a3938a93
Chris Throup <chris@throup.org.uk>
2014-12-27 23:10:46 +0000

    Added XYZ colour implementation.

---
+++ src/XyzColour.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * This file contains the XyzColour class.
+ *
+ * @author    Chris Throup <chris@throup.org.uk>
+ * @copyright 2014 Chris Throup
+ * @licence   GPL-3.0+
+ */
+
+namespace Colourspace;
+
+/**
+ * Represents a colour in an XYZ colourspace
+ */
+class XyzColour {
+    /**
+     * Instantiate a colour for the provided XYZ representation.
+     *
+     * @param float $X The X component of this colour's XYZ representation; in
+     *                 the range [0.0–1.0].
+     * @param float $Y The Y component of this colour's XYZ representation; in
+     *                 the range [0.0–1.0].
+     * @param float $Z The Z component of this colour's XYZ representation; in
+     *                 the range [0.0–1.0].
+     */
+    public function __construct($X = 0.0, $Y = 0.0, $Z = 0.0) {
+        $this->X = (float) $X;
+        $this->Y = (float) $Y;
+        $this->Z = (float) $Z;
+    }
+
+    /**
+     * Gets the X component of this colour's XYZ representation; in the
+     * range [0.0–1.0].
+     *
+     * @return float
+     */
+    public function getX() {
+        return (float) $this->X;
+    }
+
+    /**
+     * Gets the Y component of this colour's XYZ representation; in the
+     * range [0.0–1.0].
+     *
+     * @return float
+     */
+    public function getY() {
+        return (float) $this->Y;
+    }
+
+    /**
+     * Gets the Z component of this colour's XYZ representation; in the
+     * range [0.0–1.0].
+     *
+     * @return float
+     */
+    public function getZ() {
+        return (float) $this->Z;
+    }
+
+    /**
+     * Determine if this colour is equal to another.
+     *
+     * @param self $second The other colour to which this one will be compared.
+     *
+     * @return bool
+     */
+    public function equals(self $second) {
+        return ($this->getX() == $second->getX())
+            && ($this->getY() == $second->getY())
+            && ($this->getZ() == $second->getZ());
+    }
+
+    /**
+     * The X component of this colour's XYZ representation; in the
+     * range [0.0–1.0].
+     *
+     * @var float
+     */
+    private $X   = 0.0;
+
+    /**
+     * The Y component of this colour's XYZ representation; in the
+     * range [0.0–1.0].
+     *
+     * @var float
+     */
+    private $Y = 0.0;
+
+    /**
+     * The Z component of this colour's XYZ representation; in the
+     * range [0.0–1.0].
+     *
+     * @var float
+     */
+    private $Z  = 0.0;
+
+    /**
+     * Gets the red component of this colour's RGB representation; in
+     * the range [0.0–1.0].
+     *
+     * @return float
+     */
+    public function getRed() {
+        $RGB = $this->getRGB();
+        return $RGB['R'];
+    }
+
+    /**
+     * Gets the green component of this colour's XYZ representation; in
+     * the range [0.0–1.0].
+     *
+     * @return float
+     */
+    public function getGreen() {
+        $RGB = $this->getRGB();
+        return $RGB['G'];
+    }
+
+    /**
+     * Gets the blue component of this colour's XYZ representation; in
+     * the range [0.0–1.0].
+     *
+     * @return float
+     */
+    public function getBlue() {
+        $RGB = $this->getRGB();
+        return $RGB['B'];
+    }
+
+    /**
+     * Calculates the RGB representation for this colour.
+     *
+     * @return float[]|array {
+     *     @var float $R The red component of this colour's RGB
+     *                   representation; in the range [0.0–1.0].
+     *     @var float $G The green component of this colour's RGB
+     *                   representation; in the range [0.0–1.0].
+     *     @var float $B The blue component of this colour's RGB
+     *                   representation; in the range [0.0–1.0].
+     * }
+     */
+    protected function getRGB() {
+        $XYZ = new Matrix(
+            [
+                [$this->X],
+                [$this->Y],
+                [$this->Z],
+            ]
+        );
+
+        $matrix = new Matrix(
+            [
+                [ 3.2404542, -1.5371385, -0.4985314],
+                [-0.9692660,  1.8760108,  0.0415560],
+                [ 0.0556434, -0.2040259,  1.0572252],
+            ]
+        );
+
+        $RGB = $matrix->product($XYZ);
+
+        return [
+            'R' => $this->applyGamma($RGB[0][0]),
+            'G' => $this->applyGamma($RGB[1][0]),
+            'B' => $this->applyGamma($RGB[2][0]),
+        ];
+    }
+
+    /**
+     * Applies the gamma curve appropriate to this XYZ colour space.
+     *
+     * NB this initial implementation is for the sXYZ space which does
+     * something a bit funky with the smaller values. Most XYZ spaces simply
+     * apply an exponential factor.
+     *
+     * @param float $input The value to which the adjustment should be applied.
+     *
+     * @return float
+     */
+    protected function applyGamma($input) {
+        if ($input > 0.031308) {
+            $output = 1.055 * pow($input, 1/2.4) - 0.055;
+        } else {
+            $output = $input * 12.92;
+        }
+        return $output;
+    }
+}
---
+++ tests/XyzColourTest.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * This file contains test cases for the XyzColour class.
+ *
+ * @author    Chris Throup <chris@throup.org.uk>
+ * @copyright 2014 Chris Throup
+ * @licence   GPL-3.0+
+ */
+
+namespace Colourspace;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Class XyzColourTest
+ */
+class XyzColourTest extends PHPUnit_Framework_TestCase {
+    const RED_X = 0.412423758;
+    const RED_Y = 0.212656;
+    const RED_Z = 0.019332364;
+    const DELTA = 0.00005;
+
+    /**
+     * @test
+     */
+    public function constructionWithValues_setsXYZValues() {
+        $X = 0.2;
+        $Y = 0.4;
+        $Z = 0.8;
+
+        $colour = $this->generateXyzColour($X, $Y, $Z);
+
+        $this->assertEquals($X, $colour->getX());
+        $this->assertEquals($Y, $colour->getY());
+        $this->assertEquals($Z, $colour->getZ());
+    }
+
+    /**
+     * @test
+     */
+    public function equal() {
+        $first  = $this->generateXyzColour(1, 0, 0);
+        $second = $this->generateXyzColour(1, 0, 0);
+
+        $this->assertTrue($first->equals($second));
+        $this->assertTrue($second->equals($first));
+    }
+
+    /**
+     * @test
+     */
+    public function notEqual() {
+        $first  = $this->generateXyzColour(1, 0, 0);
+        $second = $this->generateXyzColour(0, 0, 1);
+
+        $this->assertFalse($first->equals($second));
+        $this->assertFalse($second->equals($first));
+    }
+
+    /**
+     * @test
+     */
+    public function getRed_returnsExpectedValues() {
+        $X = self::RED_X; $Y = self::RED_Y; $Z = self::RED_Z;
+        $R = 1;
+
+        $colour = $this->generateXyzColour($X, $Y, $Z);
+
+        $this->assertEquals($R, $colour->getRed(), '', self::DELTA);
+    }
+
+    /**
+     * @test
+     */
+    public function getGreen_returnsExpectedValues() {
+        $X = self::RED_X; $Y = self::RED_Y; $Z = self::RED_Z;
+        $G = 0;
+
+        $colour = $this->generateXyzColour($X, $Y, $Z);
+
+        $this->assertEquals($G, $colour->getGreen(), '', self::DELTA);
+    }
+
+    /**
+     * @test
+     */
+    public function getBlue_returnsExpectedValues() {
+        $X = self::RED_X; $Y = self::RED_Y; $Z = self::RED_Z;
+        $B = 0;
+
+        $colour = $this->generateXyzColour($X, $Y, $Z);
+
+        $this->assertEquals($B, $colour->getBlue(), '', self::DELTA);
+    }
+
+    /**
+     * Generate a testable colour object for the provided XYZ representation.
+     *
+     * @param float $X The X component of this colour's XYZ representation; in
+     *                 the range [0.0–1.0].
+     * @param float $Y The Y component of this colour's XYZ representation; in
+     *                 the range [0.0–1.0].
+     * @param float $Z The Z component of this colour's XYZ representation; in
+     *                 the range [0.0–1.0].
+     *
+     * @return XyzColour
+     */
+    protected function generateXyzColour($X = 0.0,
+                                         $Y = 0.0,
+                                         $Z = 0.0) {
+        return new XyzColour($X, $Y, $Z);
+    }
+}
