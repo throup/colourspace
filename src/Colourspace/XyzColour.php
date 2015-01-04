41d910d7ab22ea3c8f406daeae4c5097f4849931
Chris Throup <chris@throup.org.uk>
2015-01-04 12:41:31 +0000

    Deleted BaseColour abstract class; all functionality implemented directly within the lone child class: XyzColour.

--- src/Colourspace/XyzColour.php
+++ src/Colourspace/XyzColour.php
@@ -14,7 +14,7 @@ use Colourspace\Matrix\Matrix;
 /**
  * Represents a colour in an XYZ colourspace
  */
-class XyzColour extends BaseColour {
+class XyzColour implements Colour {
     /**
      * Instantiate a colour for the provided XYZ representation.
      *
@@ -182,4 +182,108 @@ class XyzColour extends BaseColour {
      * @var float
      */
     private $Z  = 0.0;
+
+    /**
+     * Returns the standard sRGB transformation matrix.
+     *
+     * @return Matrix
+     */
+    protected function sRGBmatrix() {
+        $W = $this->referenceWhite();
+        $R = $this->primaryRed();
+        $G = $this->primaryGreen();
+        $B = $this->primaryBlue();
+
+
+        $colours = new Matrix(
+            [
+                [$R[0][0], $G[0][0], $B[0][0]],
+                [$R[1][0], $G[1][0], $B[1][0]],
+                [$R[2][0], $G[2][0], $B[2][0]],
+            ]
+        );
+
+        $coloursI = $colours->inverse();
+
+        $S = $coloursI->product($W);
+
+        return new Matrix(
+            [
+                [$S[0][0] * $R[0][0], $S[1][0] * $G[0][0], $S[2][0] * $B[0][0]],
+                [$S[0][0] * $R[1][0], $S[1][0] * $G[1][0], $S[2][0] * $B[1][0]],
+                [$S[0][0] * $R[2][0], $S[1][0] * $G[2][0], $S[2][0] * $B[2][0]],
+            ]
+        );
+    }
+
+    /**
+     * Returns the inverse of the standard sRGB transformation matrix.
+     *
+     * @return Matrix
+     */
+    protected function sRGBinverseMatrix() {
+        $matrix = $this->sRGBmatrix();
+        return $matrix->inverse();
+    }
+
+    /**
+     * @return Matrix
+     */
+    protected function referenceWhite() {
+        $factory = new StandardIlluminantFactory();
+        $illuminant = $factory->D(65);
+
+        return new Matrix(
+            [
+                [$illuminant->getX()],
+                [$illuminant->getY()],
+                [$illuminant->getZ()],
+            ]
+        );
+    }
+
+    /**
+     * @return Matrix
+     */
+    protected function primaryRed() {
+        $colourspace = new xyYColourspace();
+        $colour = $colourspace->generate(0.6400, 0.3300, 1);
+        return new Matrix(
+            [
+                [$colour->getX()],
+                [$colour->getY()],
+                [$colour->getZ()],
+            ]
+        );
+    }
+
+    /**
+     * @return Matrix
+     */
+    protected function primaryGreen() {
+        $colourspace = new xyYColourspace();
+        $colour = $colourspace->generate(0.3000, 0.6000, 1);
+        return new Matrix(
+            [
+                [$colour->getX()],
+                [$colour->getY()],
+                [$colour->getZ()],
+            ]
+        );
+    }
+
+    /**
+     * @return Matrix
+     */
+    protected function primaryBlue() {
+        $colourspace = new xyYColourspace();
+        $colour = $colourspace->generate(0.1500, 0.0600, 1);
+        return new Matrix(
+            [
+                [$colour->getX()],
+                [$colour->getY()],
+                [$colour->getZ()],
+            ]
+        );
+    }
 }
